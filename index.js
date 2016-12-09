a0526e551ddccd61f4cbba0452eda58602d3aaea
dustinrue <ruedu@dustinrue.com>
2016-12-09 17:31:29 -0600

    initial

---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var express = require('express');
+var fs = require('fs');
+var url = require('url');
+var routes = require('./routes');
+var sqlite3 = require('sqlite3').verbose();
+var https = require('https');
+var querystring = require('querystring');
+var db = new sqlite3.Database(':memory:');
+
+
+var apiKey = '9b9d966677e44029b28da69312b960ed';
+var bungieInitialTokenURL = '/en/Application/Authorize/10767';
+var tokens = '';
+
+var app = express();
+
+// the username is always RealAngryMonkey
+app.get('/', function(req, res) {
+  db.serialize(function() {
+    db.get("SELECT username, token, refreshToken FROM token WHERE username = 'RealAngryMonkey'", function(err, row) {
+      if (typeof(row) === "undefined") {
+        res.redirect(302, 'https://www.bungie.net' + bungieInitialTokenURL + "?state=test");
+        /*
+        res.send("Adding user<br/>");
+        try {
+	  db.run("INSERT INTO token (username) VALUES($username)", { $username: 'RealAngryMonkey' });
+        }
+        catch (e) {
+          console.log("Failed to create user");
+          console.log(e);
+        }
+        */
+      }
+      else {
+        res.send(row);
+      }
+    });
+  });
+});
+
+app.get('/initial', function(req, res) {
+  var url_parts = url.parse(req.url, true);
+  var code = url_parts.query.code;
+  var response = '';
+
+  var post_data = {
+    'code': code,
+  }
+  post_data = JSON.stringify(post_data);
+
+  var request_options = {
+    hostname: 'www.bungie.net',
+    path: '/Platform/App/GetAccessTokensFromCode/',
+    method: 'POST',
+    headers: {
+      'X-API-Key': apiKey,
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Content-Length': Buffer.byteLength(post_data)
+    }
+
+  }
+  var post_req = https.request(request_options, function(tokenRes) {
+    tokenRes.setEncoding('utf8');
+    tokenRes.on('data', function(chunk) {
+      tokens += chunk;
+    });
+    tokenRes.on('end', function() {
+      res.send(tokens);
+    });
+  });
+
+  post_req.write(post_data);
+  post_req.end();
+
+});
+
+// http://blog.mgechev.com/2014/02/19/create-https-tls-ssl-application-with-express-nodejs/
+https.createServer({
+  key: fs.readFileSync('ssl/key.pem'),
+  cert: fs.readFileSync('ssl/cert.pem')
+}, app).listen(3000, function() {
+  db.serialize(function() {
+    db.run("CREATE TABLE token (username TEXT, token TEXT, refreshToken TEXT)");
+  });
+  
+  console.log("Ready on port 3000");
+});
